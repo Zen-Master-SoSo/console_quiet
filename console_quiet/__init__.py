aff3166528ea9053dd719ae6e51ca57b65fb8689
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2025-07-30 16:33:21 +0800

    Prepare for publish

--- console_quiet/__init__.py
+++ console_quiet/__init__.py
@@ -2,10 +2,31 @@
 #
 #  Copyright 2024 liyang <liyang@veronica>
 #
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+"""
+Provides a context manager for doing a "deep suppression" of stdout and stderr.
+"""
 import os
 
+__version__ = "1.0.0"
+
+
 class ConsoleQuiet(object):
-	'''
+	"""
 	A context manager for doing a "deep suppression" of stdout and stderr in
 	Python, i.e. will suppress all print, even if the print originates in a
 	compiled C/Fortran sub-function.
@@ -13,7 +34,7 @@ class ConsoleQuiet(object):
 	to stderr just before a script exits, and after the context manager has
 	exited (at least, I think that is why it lets exceptions through).
 	https://stackoverflow.com/questions/11130156/suppress-stdout-stderr-print-from-python-functions
-	'''
+	"""
 
 	def __init__(self):
 		# Open a pair of null files
